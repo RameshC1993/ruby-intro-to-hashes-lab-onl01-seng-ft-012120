ab2cc4c928b44f38cabf27877f68e822d673dfe3
Ramesh Chuwan <chhetrirahul2015@gmail.com>
2020-01-29 04:38:56 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -57,13 +57,13 @@ def monopoly_with_fourth_tier
 	        "mortgage_value" => "$100"
 	      },
 	      :pennsylvania_railroad => {
-	        "mortagage_value" => "$200"
+	        "mortgage_value" => "$200"
 	      },
 	      :b_and_o_railroad => {
-	        "mortagage_value" => "$400"
+	        "mortgage_value" => "$400"
 	      },
 	      :shortline => {
-	        "mortagage_value" => "$800"
+	        "mortgage_value" => "$800"
 	      },
 	    },
 	    :rent_in_dollars => {
